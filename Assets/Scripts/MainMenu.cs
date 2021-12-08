67d11338884664ce21d01762a70cc66fff5d937a
caseyjarmes <44095299+caseyjarmes@users.noreply.github.com>
2021-12-07 23:07:02 -0600

    Some walls for Downtown, tried to fix main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,23 +8,34 @@ using TMPro;
 public class MainMenu : MonoBehaviour
 {
     public Button Selected;
+    public TMP_Dropdown trackSelect;
+
     // Start is called before the first frame update
     void Start()
     {
         Selected.Select();
+
     }
     public void PlayGame()
     {
-        SceneManager.LoadScene("Speedway");
+        if (trackSelect.value == 0)
+        {
+            SceneManager.LoadScene("Speedway");
+        }
+        else if (trackSelect.value == 1)
+        {
+            SceneManager.LoadScene("Crashsite");
+        }
+        else if (trackSelect.value == 2)
+        {
+            SceneManager.LoadScene("Downtown");
+        }
     }
     public void ExitGame()
     {
         Application.Quit();
     }
-    public void Survey()
-    {
-        Application.OpenURL("https://forms.gle/zqw7EjyC7Vi57zQ56");
-    }
+
     public void Credits()
     {
 
--- Assets/Scripts/MainMenuCameraScript.cs
+++ Assets/Scripts/MainMenuCameraScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MainMenuCameraScript : MonoBehaviour
 {
-    public int rotationSpeed;
+    public float rotationSpeed;
 
     // Start is called before the first frame update
     void Start()
