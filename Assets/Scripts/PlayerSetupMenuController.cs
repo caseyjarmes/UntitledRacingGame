9e82e54f0f608b5c3a66e82887b87368c43e83fd
pjhealey507 <74697651+pjhealey507@users.noreply.github.com>
2022-02-18 14:24:26 -0600

    Adds Multiplayer Menu

---
+++ Assets/Scripts/PlayerSetupMenuController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerSetupMenuController : MonoBehaviour
+{
+    private int playerIndex;
+
+    [SerializeField]
+    private TextMeshProUGUI titleText;
+    [SerializeField]
+    private GameObject readyPanel;
+    [SerializeField]
+    private GameObject menuPanel;
+    [SerializeField]
+    private Button readyButton;
+
+    private float ignoreInputTime = 1.5f;
+    private bool inputEnabled;
+    public void setPlayerIndex(int pi)
+    {
+        playerIndex = pi;
+        titleText.SetText("Player " + (pi + 1).ToString());
+        ignoreInputTime = Time.time + ignoreInputTime;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputEnabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > ignoreInputTime)
+        {
+            inputEnabled = true;
+        }
+    }
+
+    //parameter needs to be changed to some other type that represents the ship
+    //then changed in the editor under the button objects
+    public void SelectColor() //(Material mat)
+    {
+        //if (!inputEnabled) { return; }
+
+        //remove this
+        Material mat = null;
+
+        //SET PLAYER SHIP
+        //requires some work in the buttons in the editor, 32 mins in video => https://www.youtube.com/watch?v=_5pOiYHJgl0
+        PlayerConfigurationManager.Instance.SetPlayerColor(playerIndex, mat);
+        readyPanel.SetActive(true);
+        readyButton.interactable = true;
+        menuPanel.SetActive(false);
+        readyButton.Select();
+
+    }
+
+    public void ReadyPlayer()
+    {
+        if (!inputEnabled) { return; }
+
+        PlayerConfigurationManager.Instance.ReadyPlayer(playerIndex);
+        readyButton.gameObject.SetActive(false);
+    }
+}
