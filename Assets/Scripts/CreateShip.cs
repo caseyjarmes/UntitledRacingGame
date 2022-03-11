d94b28fbf52838e4e9fe03c04222771377e45fe4
pjhealey507 <74697651+pjhealey507@users.noreply.github.com>
2022-03-11 10:55:29 -0600

    Adds camera fix and ship selection to all maps

--- Assets/Scripts/CreateShip.cs
+++ Assets/Scripts/CreateShip.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CreateShip : MonoBehaviour
 {
     public List<GameObject> ships = new List<GameObject>();
+    public GameObject CameraHandler;
 
     // Start is called before the first frame update
     void Start()
@@ -14,7 +15,20 @@ public class CreateShip : MonoBehaviour
 
     public void InstantiateShip(int chosen_ship)
     {
-        Instantiate(ships[chosen_ship], new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z), Quaternion.identity);
+        GameObject ship_reference;
+
+        ship_reference = (GameObject)Instantiate(ships[chosen_ship], new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z), Quaternion.Euler(0, transform.eulerAngles.y, 0));
+
+        SetUpCamera(ship_reference);
     }
-    
+
+    public void SetUpCamera(GameObject ship_reference)
+    {
+        //for camera movement (during accel)
+        CameraHandler.GetComponent<CinemachineManipulator>().CarControl = ship_reference.GetComponent<CarControl>();
+
+        //camera follows ship
+        CameraHandler.GetComponentInChildren<Cinemachine.CinemachineVirtualCamera>().Follow = ship_reference.transform;
+        CameraHandler.GetComponentInChildren<Cinemachine.CinemachineVirtualCamera>().LookAt = ship_reference.transform.GetChild(1);
+    }
 }
