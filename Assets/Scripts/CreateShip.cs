f3a875b053f37771ad0b1e186f5a200ecb836e78
pjhealey507 <74697651+pjhealey507@users.noreply.github.com>
2022-04-08 14:00:02 -0500

    Fixes issue with Great White getting caught on walls

--- Assets/Scripts/CreateShip.cs
+++ Assets/Scripts/CreateShip.cs
@@ -20,7 +20,7 @@ public class CreateShip : MonoBehaviour
     public float speed_mult = 1f;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         InstantiateShip(ShipChoice.ShipIndex);
     }
