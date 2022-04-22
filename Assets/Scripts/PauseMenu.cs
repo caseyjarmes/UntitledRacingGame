f6a16ad592f6f1fc393a6da913e8fa709ed367d7
pjhealey507 <74697651+pjhealey507@users.noreply.github.com>
2022-04-22 13:32:21 -0500

    Adds controller support to pause menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool game_paused = false;
 
     public GameObject pause_menu_ui;
+    public Button resume_button;
 
     // Update is called once per frame
     void Update()
     {
-        //refactor to be used with action map for controller support
-        if (Input.GetKeyDown(KeyCode.Escape))
+        //refactor to be used with action map for cleaner controller support
+        if (Input.GetKeyDown(KeyCode.Escape) || Gamepad.current.startButton.wasPressedThisFrame)
         {
             if (game_paused)
             {
@@ -29,6 +32,7 @@ public class PauseMenu : MonoBehaviour
     void Pause()
     {
         pause_menu_ui.SetActive(true);
+        resume_button.Select();
         Time.timeScale = 0f;
         game_paused = true;
     }
